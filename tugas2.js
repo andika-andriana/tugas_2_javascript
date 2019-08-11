6f893e52aa59ce320c63b80857612d535ebd25f7
andika-andriana <andriana.mia@gmail.com>
2019-08-11 15:01:15 +0700

    Tugas 2 Javascript
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var number = 24;
+var string = "Andika"
+var boolean = true;
+var nulL = null;
+var undifineD;
+var array = [5, 4, 3, 2, 1];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(nulL);
+console.log(undifineD);
+console.log(array);
